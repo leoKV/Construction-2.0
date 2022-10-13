3b9f6689a96b6d0005b6303054823c624ca0fab4
leoKV <kevorvs@gmail.com>
2022-10-13 15:36:24 -0500

    Primer Commit

---
+++ Construction/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Construction.Models.ViewModel;
+using Rotativa;
+
+namespace Construction.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Report()
+        {
+            List<PersonaViewModel> lst = new List<PersonaViewModel>();
+            lst.Add(new PersonaViewModel() { Nombre="Héctor",Edad=31,Email="algo@gmail.com"});
+            lst.Add(new PersonaViewModel() { Nombre = "Héctor 2", Edad = 31, Email = "algo@gmail.com" });
+            lst.Add(new PersonaViewModel() { Nombre = "Héctor 3", Edad = 31, Email = "algo@gmail.com" });
+            lst.Add(new PersonaViewModel() { Nombre = "Héctor 4", Edad = 31, Email = "algo@gmail.com" });
+            lst.Add(new PersonaViewModel() { Nombre = "Héctor 5", Edad = 31, Email = "algo@gmail.com" });
+            return View(lst);
+        }
+
+
+        public ActionResult Print()
+        {
+            return new ActionAsPdf("Report")
+            { FileName = "Reporte.pdf" };
+        }
+
+        public ActionResult PrintHome()
+        {
+            return new ActionAsPdf("Index")
+            { FileName = "MainPage.pdf" };
+        }
+    }
+}
---
+++ Construction/Models/ViewModel/PersonaViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Construction.Models.ViewModel
+{
+    public class PersonaViewModel
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Email { get; set; }
+
+    }
+}
